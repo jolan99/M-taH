9921c2d5b774b1573d6ecc1680224b5c2f43be0a
jolan99 <anna.joliot@etu.u-bordeaux.fr>
2023-02-17 12:26:30 +0100

    enumeration finie

--- enumeration.cpp
+++ enumeration.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstring>
 #include <queue>
+#include<limits>
 using namespace std;
 
 
@@ -16,6 +17,12 @@ struct arc {
 
 typedef vector<arc> graphe;
 
+struct donnees {
+    graphe graphe;
+    int nb_sommets;
+};
+
+
 /*graphe init_graphe( int n){
     unordered_map<int,arc> v;            //on cree nos sommets sans arcs 
     graphe gr;
@@ -26,6 +33,7 @@ typedef vector<arc> graphe;
 }*/
 
 /*fonction permettant d'ajouter un arc a ayant pour prédécesseur le noeud p, le successeur s au graphe gr */
+
 void add_arc (arc &a, graphe &gr){  
         gr.push_back(a);   //on ajoute l'arc a à la liste d'arc =
 }
@@ -51,7 +59,8 @@ void print_graph(graphe &gr){
 }
 
 
-graphe read_file (string str){
+donnees read_file (string str){
+    donnees data;
     ifstream file (str);
     int nb_sommet;
     int nb_arrete;
@@ -79,7 +88,7 @@ graphe read_file (string str){
                 continue;
             }
             if (compteur == 1){
-                nb_sommet = stoi(first_word);
+                data.nb_sommets = stoi(first_word);
                 nb_arrete = stoi(second_word);
                 //gr = init_graphe(nb_sommet);
             }
@@ -101,12 +110,14 @@ graphe read_file (string str){
             }
         }
         
-        return gr;  
+        //return gr;  
     }
     else{
     cout << "ERREUR: Impossible d'ouvrir le fichier en lecture."  << endl;
     }
-    return gr;
+    data.graphe = gr;
+    
+    return data;
 }
 
 // de moi : 
@@ -122,33 +133,209 @@ solution init_solution(int nb_classes){
 }
 
 
+pair<bool,int> is_in_group(solution sol, int sommet){ // regarde si un sommet est déjà dans mis dans un groupe et lequel
+    bool verif = false;
+    int num_groupe = -1;
+    int iterateur = 1;
+    pair<bool,int> resultat;
+    for(auto it = sol.begin(); it!= sol.end(); ++it){ // on énumère sur tous les groupes
+        vector<int> groupe = *it;
+        for(auto is = groupe.begin(); is!= groupe.end(); ++is){ // on énumère sur tous les membres du groupe
+            if(sommet == *is){
+                verif = true;
+                num_groupe = iterateur ;
+            }
+        }
+        iterateur +=1;
+    }
+    resultat.first = verif;
+    resultat.second = num_groupe;
+    return resultat;
+}
+
+bool is_valid(solution sol, donnees data,int nb_classes){
+    bool resultat = true;
+    int nb_sommets = data.nb_sommets;
+    
+    //graphe gr = data.graphe;
+    // tous les  sommets sont dans la solution
+    //vector<bool> condition1(false,nb_sommets);
+    for(int i=0; i < nb_sommets; ++i){
+        if(!(is_in_group(sol, i+1).first)){ // si un sommet n'est pas dans la solution, alors pas réalisable
+            resultat = false;
+        }
+    }
+    // respecte la condition d'équilibre 
+    for(int i = 0; i< nb_classes; ++i){
+        if(sol[i].size() >= 4){
+            resultat = false;
+        }
+    }
+    return resultat;
+}
+
+float valeur(solution sol, donnees data){
+    graphe gr = data.graphe;
+    int sommet1;
+    int sommet2;
+    float valeur_sol = 0;
+    for(auto it = gr.begin(); it!= gr.end(); ++it){
+        arc a = *it;
+        sommet1 = a.predecesseur;
+        sommet2 = a.voisin;
+        if(is_in_group(sol, sommet1).second != is_in_group(sol, sommet2).second){
+            valeur_sol += a.cost;
+        }
+    }
+    return valeur_sol;
+}
+
+void print_solution(pair<solution,float> resultat){
+    solution sol = resultat.first;
+    float value = resultat.second;
+    int i = 1;
+    for(auto it = sol.begin(); it!= sol.end(); ++it){
+        vector<int> groupe = *it;
+        cout << "Dans le groupe "<< i<< " se trouvent les sommets ";
+        for(auto is = groupe.begin(); is!= groupe.end(); ++is){
+            cout<< *is << " " ;
+        }
+        i +=1;
+        cout << " "<< endl;
+    }
+    cout << " "<< endl;
+    cout << " Cette solution a pour valeur " << value<< endl;
+}
 
 
-solution enumeration(graphe gr, int nb_classes){
+pair<solution,float> enumeration(donnees data, int nb_classes){
+    pair<solution,float> resultat;
+    graphe gr=data.graphe;
     queue<solution> L; // va stocker les sous_solutions au fur et à mesure
+    solution initialisation = init_solution(nb_classes);
+    L.push(initialisation);
     queue<solution> S; // va stocker les solutions au fur et à mesure
     // la queue S au dessus est-elle vraiment necessaire? ça prend de la place, et
     // on ne veut que la solution optimale à la fin 
     solution solution_initiale = init_solution(nb_classes);
     solution sBest; // va stocker la meilleure solution
+    float best_value = std::numeric_limits< float >::infinity();
+    solution s_parent;
+    solution s_enfant;
     while(!(L.empty())){
-        solution s_parent = L.front(); // on étudie une sous-solution de L 
-        for(auto it = gr.begin(); it!= gr.end(); ++it){ // on parcourt tous les arcs du graphe
-        arc a= *it;
-        int pred = a.predecesseur;
-        int voisin = a.voisin;
+        // cout<< "debeug1 " << endl;
+        s_parent = L.front(); // on étudie une sous-solution de L 
+        // cout << "##################"<< endl;
+        // cout <<"solution parent : "<< endl;
+        // print_solution(s_parent);
+        // cout << "##################"<< endl;
         
+        L.pop();
+        for(auto it = gr.begin(); it!= gr.end(); ++it){ // on parcourt tous les arcs du graphe
+            arc a = *it;
+            // cout << "-----------------------"<< endl;
+            // cout<< "on étudie l'arc " ;
+            // print_arc(a);
+            int pred = a.predecesseur;
+            int voisin = a.voisin;
+            // les trois cas possibles : 
+            // si le predecesseur n'est pas encore dans un groupe
+            if(!(is_in_group(s_parent, pred).first) && is_in_group(s_parent, voisin).first){
+                //cout << "cas 1 " << endl;
+                for(int i = 0; i<nb_classes; ++i){ //On crée toutes les combinaisons possibles, pred peut être dans tous les groupes possibles
+                    s_enfant = s_parent;
+                    s_enfant[i].push_back(pred);
+                    // cout << "solution enfant : "<< endl;
+                    // print_solution(s_enfant);
+                    float value_sol = valeur(s_enfant,data);
+                    if(is_valid(s_enfant,data,nb_classes)){
+                        if(value_sol < best_value){
+                            // on garde la solution ssi elle est meilleure que la sBest précédente. A la fin, 
+                            // elle donnera donc la valeur optimale
+                            best_value = value_sol;
+                            sBest = s_enfant;
+                        }
+                        
+                    }
+                    else{L.push(s_enfant);}
+
+                }
+            }
+            // si le voisin n'est pas encore dans un groupe
+            if(!(is_in_group(s_parent, voisin).first) && is_in_group(s_parent, pred).first){
+                // cout << "cas 2 " << endl;
+                for(int i = 0; i < nb_classes; ++i){ //On crée toutes les combinaisons possibles, pred peut être dans tous les groupes possibles
+                    s_enfant = s_parent;
+                    s_enfant[i].push_back(voisin);
+                    // cout << "solution enfant : "<< endl;
+                    // print_solution(s_enfant);
+                    float value_sol = valeur(s_enfant,data);
+                    if(is_valid(s_enfant,data,nb_classes)){
+                        if(value_sol < best_value){
+                            // on garde la solution ssi elle est meilleure que la sBest précédente. A la fin, 
+                            // elle donnera donc la valeur optimale
+                            best_value = value_sol;
+                            sBest = s_enfant;
+                        }
+                        
+                    }
+                    else{L.push(s_enfant);}
+
+                }
+            }
+            // si ni le voisin ni le predecesseur n'ont été assigné à un groupe
+            if(!(is_in_group(s_parent, pred).first) && !(is_in_group(s_parent, voisin).first)){
+                // cout << "cas 3 " << endl;
+                for(int a = 0; a < nb_classes; ++a){
+                    s_enfant = s_parent;
+                    s_enfant[a].push_back(pred);
+                    solution s_enfantbis = s_enfant;
+                    for(int b = 0; b < nb_classes; ++b){
+                        s_enfantbis = s_enfant;
+                        s_enfantbis[b].push_back(voisin);
+                        // cout << "solution enfant : ";
+                        // print_solution(s_enfantbis);
+                        float value_sol = valeur(s_enfantbis,data);
+                        if(is_valid(s_enfantbis,data,nb_classes)){
+                            if(value_sol < best_value){
+                                // on garde la solution ssi elle est meilleure que la sBest précédente. A la fin, 
+                                // elle donnera donc la valeur optimale
+                                best_value = value_sol;
+                                sBest = s_enfantbis;
+                            }
+                        }
+                        else{L.push(s_enfantbis);}
+                    }
+                }
+            }
 
         }
 
     }
-    return sBest;
+    resultat.first = sBest;
+    resultat.second = best_value;
+    return resultat;
 }
 
 
 
 int main(){
-    graphe gr = read_file("data/cinqSommets.txt");
-    print_graph(gr);
+    donnees data = read_file("data/cinqSommets.txt");
+    //print_graph(gr);
+    //solution sol={{1,2},{3,4}};
+    // cout << is_in_group(sol, 7).first << endl;
+    // donnees data;
+    // graphe gr;
+    // arc a = {1,2,30};
+    // add_arc(a,gr);
+    // arc b = {1,3,10};
+    // add_arc(b,gr);
+    // arc c = {3,4,5};
+    // add_arc(c,gr);
+    // data.graphe = gr;
+    // data.nb_sommets = 4;
+    // //cout << "valeur solution : " << valeur(sol, data);
+    print_solution(enumeration(data,4));
+    
     return 0;
 }
